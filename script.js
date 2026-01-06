dd6d62b3f1a8f3b2253a4834883df7e3de205b35
khairfirza <86790664+khairfirza@users.noreply.github.com>
2026-01-06 19:04:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 function checkCode() {
     const correctCode = "K2026";
-    const videoURL = "https://www.youtube.com/embed/Qz_YPmpfzjY";
+    const videoURL = "https://youtu.be/Qz_YPmpfzjY?si=WsoVhtwGcE0B6D-8";
 
-    const input = document.getElementById("codeInput").value.toUpperCase();
+    const input = document.getElementById("codeInput");
     const message = document.getElementById("message");
+    const correctSound = document.getElementById("correctSound");
+    const wrongSound = document.getElementById("wrongSound");
 
-    if (input === correctCode) {
+    const value = input.value.trim().toUpperCase();
+
+    if (value === correctCode) {
         message.style.color = "lightgreen";
         message.textContent = "Correct! Opening video...";
-        window.location.href = videoURL;
+        correctSound.play();
+
+        setTimeout(() => {
+            window.location.href = videoURL;
+        }, 800);
     } else {
         message.style.color = "red";
         message.textContent = "Try again";
+
+        wrongSound.play();
+
+        input.classList.add("shake");
+        setTimeout(() => {
+            input.classList.remove("shake");
+        }, 400);
     }
 }
